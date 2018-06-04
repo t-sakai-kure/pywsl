51af8bbdb27cfefcc7f5cb29835c499f381d107f
t-sakai-kure <t.sakai.kure@gmail.com>
2018-06-04 21:33:55 +0900

    [add] demo_upu.py

---
+++ examples/pul/upu/demo_upu.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+from sklearn.model_selection import GridSearchCV, StratifiedKFold
+from sklearn.utils.estimator_checks import check_estimator
+
+from pywsl.pul import pu_mr
+from pywsl.utils.syndata import gen_twonorm_pu
+from pywsl.utils.comcalc import bin_clf_err
+
+
+def main():
+    prior = .5
+    x, y, x_t, y_t = gen_twonorm_pu(n_p=30, n_u=200, 
+                                    prior_u=prior, n_t=100)
+
+    lambda_list = np.logspace(-3, 1, 5)
+    param_grid = {'prior': [prior], 
+                  'lam': np.logspace(-3, 1, 5), 
+                  'basis': ['lm']}
+    clf = GridSearchCV(estimator=pu_mr.PU_SL(), 
+                       param_grid=param_grid,
+                       cv=5, n_jobs=-1)
+    clf.fit(x, y)
+    y_h = clf.predict(x_t)
+    err = 100*bin_clf_err(y_h, y_t, prior)
+    print("MR: {:.2f}%".format(err))
+
+
+if __name__ == "__main__":
+    main()
